67354ebc96127eadb744249a853f2584c4f01bca
Ana Lídia <142053673+analidiac@users.noreply.github.com>
2023-08-22 14:36:45 -0300

    Add files via upload

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dados 
+// booleanos
+
+// convers]ao implícita
+const numero = 456;
+const numeroString = Number("456a");
+
+//Number()
+//String()
+console.log(numero + numeroString)
+
+// conversão explícita
+
